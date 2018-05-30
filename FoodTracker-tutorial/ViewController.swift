38775f2cf7205d6080461cc3aa83cbe7b74d0570
Paul Choi <q1431708@mac1254903.qvcdev.qvc.net>
2018-05-30 14:40:23 -0400

    Initial Commit

---
+++ FoodTracker-tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker-tutorial
+//
+//  Created by Paul Choi on 5/30/18.
+//  Copyright © 2018 Paul Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
